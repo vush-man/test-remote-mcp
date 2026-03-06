1bad8eced3202ea8733914fcf3a3b3717d3b2e22
Ayan Goswami <144007498+vush-man@users.noreply.github.com>
2026-03-06 10:52:19 +0530

    Update expense_tracker_remote.py

--- expense_tracker_remote.py
+++ expense_tracker_remote.py
@@ -138,5 +138,5 @@ def categories():
 
 # Start the server
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=8000)
+    mcp.run(transport="http", host="0.0.0.0", port=int(os.environ.get("PORT", 8000))
     # mcp.run()
